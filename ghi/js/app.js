a1d7a09f6649c18fa974058f3c5ae94957c36c02
Garrett Hoffman <son0hoff@gmail.com>
2023-02-22 12:25:10 -0800

    Update for Basic Fetch and updating webpage main body content

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,32 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
+    const url = 'http://localhost:8000/api/conferences/';
 
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+          const data = await response.json();
+
+          const conference = data.conferences[0];
+          const nameTag = document.querySelector('.card-title');
+          nameTag.innerHTML = conference.name;
+
+          const detailURL =  `http://localhost:8000${conference.href}`;
+          const detailResponse = await fetch(detailURL);
+          if (detailResponse.ok) {
+            const details = await detailResponse.json();
+
+            const textTag = document.querySelector('.card-text');
+            textTag.innerHTML = details.conference.description;
+
+            const imgTag = document.querySelector('.card-img-top');
+            imgTag.src = details.conference.location.picture_url;
+          }
+        } else {
+          throw new Error('Response not ok');
+        }
+      } catch (e) {
+            console.error('error', e);
+      }
 
 });
